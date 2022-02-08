49d429e2cb596909ee3aa2eb40881536c24d090b
DmitrYudin <dmitr.yudin@mail.ru>
2022-02-08 10:57:40 +0300

    DB stable version

--- app/client_controller.py
+++ app/client_controller.py
@@ -1,10 +1,13 @@
 from app import app
+from flask import request
+import json
 
 
 
-
-@app.route('/client/registration')
+@app.route('/client/registration', methods=['GET', 'POST'])
 def client_registration():
+    d = json.loads(request.get_data().decode('utf-8'))
+    print(d)
     return 'Hello World!'
 
 
@@ -14,10 +17,10 @@ def client_login():
 
 
 @app.route('/client/edit_profile')
-def client_profile():
+def client_edit_profile():
     return 'Hello World!'
 
 
-@app.route('/client/get_profile')
-def client_profile():
+@app.route('/client/get_profile', methods=['GET', 'POST'])
+def client_get_profile():
     return 'Hello World!'
--- app/model.py
+++ app/model.py
@@ -17,6 +17,42 @@ class Client(db.Model):
    	date_of_registration = db.Column(db.DateTime(120), index = True, unique = True)
    	is_verified = db.Column(db.String(120), index = True, unique = True)
 
+class Owner(db.Model):
+    pass
+
+
+
+class Realtor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)                        #– название организации;
+    inn = db.Column(db.String(120), index = True, unique = True)        #– ИНН (проверка в налоговой);
+    address = db.Column(db.String(120), index = True, unique = False)   #– юр. адрес;                                                              
+    contact = db.Column(db.String(120), index = True, unique = False)   #– контактное лицо;
+    manager = b.Column(db.String(120), index = True, unique = False)    #-
+    name = db.Column(db.String(120), index = True, unique = False)
+    email = db.Column(db.String(120), index = True, unique = False)     #– адрес эл. почты (проверка путем отправки ссылки);
+    phone = db.Column(db.String(120), index = True, unique = False)     #– сотовый телефон (проверка путем отправки кода в смс);
+    password_hash = db.Column(db.String(120), index = True, unique = False)    #– пароль (подтверждение пароля);
+    date_of_registration = db.Column(db.DateTime(120), index = True, unique = True)
+    qr = db.Column(db.String(120), index = True, unique = True)
+    token = db.Column(db.String(120), index = True, unique = True)
+    
+    pass
+
+
+
+
+
+– регистрационные данные (название организации, ф.и.о. директора, ИНН, адрес юр. лица, сколько лет зарегистрирована в системе);
+– количество сдаваемого жилья (кол-во квартир, сдаваемых за год);
+– срок нахождения в системе;
+– рейтинг риелтора, составленный в стороннем сервисе;
+– рейтинг риелтора, составленный на основе оценок арендаторов (8-10 – зеленый, 5-7 – желтый, 0-4 – красный);
+– история сдачи жилого помещения (одного или нескольких) – заполняется автоматически при сдаче жилых помещений и регистрации договора;
+– срок нахождения в системе;
+– рейтинг (по 10 бальной шкале);
+– отзывы о риелторе;
+– возможность редактирования всех регистрационных данных (добавление фото) с их повторной проверкой;
+
 
    
     
